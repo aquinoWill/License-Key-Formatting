3d119837270c71607687e7304ee27eee5e49fffd
Willian Aquino <willian.aquino@iterative.com.br>
2023-11-30 18:03:29 -0300

    Remove build files

--- src/parseCode.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseCode = void 0;
-var parseCode = function (code, maxValue) {
-    var splitVal = code.split('-');
-    console.log(splitVal[1].substring(maxValue));
-};
-exports.parseCode = parseCode;
